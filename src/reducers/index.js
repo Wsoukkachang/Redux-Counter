63e3da94cccc5e9c9c7048b74264af94098bafdc
William Soukkachang <WSoukkachang@yahoo.com>
2019-04-24 21:02:20 -0500

    Added redux dev extension, filled in reducer and action functions. Completed onClick functions. Completed MVP.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { INCREMENT, DECREMENT } from '../actions';
+// import { settings } from 'cluster';
 
 const initialState = {
   count: 0
@@ -12,9 +13,21 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+
+    return{
+      ...state,
+      count: state.count + 1
+      // Fill in the body of this case
+    }
+
     case DECREMENT:
-    // Fill in the body of this case
+
+    return{
+      ...state,
+      count: state.count - 1
+      // Fill in the body of this case
+    }
+
     default:
       return state;
   }
